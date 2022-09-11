8ddb9d1bb562dd78f757c8fa6df28214037e7368
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-09-11 12:17:18 +0300

    fix card props

--- src/components/benefit-card/benefit-card.jsx
+++ src/components/benefit-card/benefit-card.jsx
@@ -4,15 +4,33 @@ import './style.css';
 import Title3 from '../title3/title3';
 import Category from "../category/category";
 
-export default function BenefitCard({category, color, title, text, image}) {
+export default function BenefitCard({color, image, category, title, text}) {
   return (
-    <article className="benefit-card" style={{backgroundColor: color}}>
-      <img src={image} width={52} height={52} alt="benefit" />
-      <div className="benefit-card__header">
-        <Category category={category} />
-        <Title3>{title}</Title3>
+    <article 
+      className="benefit-card" 
+      style={{backgroundColor: color}}
+    >
+      <img 
+        src={image} 
+        width={52} 
+        height={52} 
+        alt="benefit" 
+      />
+      <div 
+        className="benefit-card__header"
+      >
+        <Category 
+          category={category} 
+        />
+        <Title3>
+          {title}
+        </Title3>
       </div>
-      <p className="benefit-card__text" dangerouslySetInnerHTML={{__html: text}} />
+      <p
+        className="benefit-card__text" 
+      >
+        {text}
+      </p>
     </article>
   );
 }
